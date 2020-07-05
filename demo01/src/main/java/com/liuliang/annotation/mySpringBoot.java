4db3bf4bff69642231e88aa9317490f8be6d2fc6
liuliangys <liuliangys1991@163.com>
2020-07-05 22:41:04 +0800

    myfirst commit

---
+++ demo01/src/main/java/com/liuliang/annotation/mySpringBoot.java
@@ -0,0 +1,12 @@
+package com.liuliang.annotation;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mySpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(mySpringBoot.class,args);
+    }
+}
